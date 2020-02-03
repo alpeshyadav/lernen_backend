59eb1709466060a94f51dfb3c8c8409d51f2e293
Alpesh Yadav <40252219+alpeshyadav@users.noreply.github.com>
2020-02-03 22:50:34 +0530

    Delete config.py

--- learn/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config(object):
-    DEBUG = True
-    SECRET_KEY = os.urandom(12)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Alpesh@4599@localhost:5432/postgres"
